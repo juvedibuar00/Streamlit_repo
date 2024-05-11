0e4acd53bd6580bac78a864c2e4ae3bba4c4984a
juvedibuar00 <130672236+juvedibuar00@users.noreply.github.com>
2024-05-11 21:59:51 +0000

    atualizando

---
+++ pages/nome.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+tab1, tab2, tab3 = st.tabs(['pagina 1', 'pagina 2', 'pagina 3'])
+
+
+with tab3:
+    st.map()
+
+
+
+
+tab4, tab5 = st.tabs(['pagina 4', 'pagina 5'])
+
+with tab4:
+    st.video('https://www.youtube.com/watch?v=ncUzxOFibgA')
+
+with tab5:
+    st.image('https://images.pexels.com/photos/20147082/pexels-photo-20147082/free-photo-of-neve-leve-luz-light.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
